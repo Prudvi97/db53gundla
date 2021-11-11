6ca6f9cb0123f909464c60ec17c7c0e94f6b7a9d
Prudvi97 <89538003+Prudvi97@users.noreply.github.com>
2021-11-10 23:38:28 -0600

    last

--- controllers/pastry.js
+++ controllers/pastry.js
@@ -7,8 +7,24 @@ exports.pastry_detail = function(req, res) {
 }; 
  
 // Handle pastry create on POST. 
-exports.pastry_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: pastry create POST'); 
+exports.pastry_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new Pastry(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.pastry = req.body.pastry; 
+    document.price = req.body.price; 
+    document.toppings = req.body.toppings; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // Handle pastry delete form on DELETE. 
@@ -27,6 +43,19 @@ exports.pastry_list = async function(req, res) {
     try{ 
         thepastrys = await Pastry.find(); 
         res.send(thepastrys); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
+
+// VIEWS 
+// Handle a show all view 
+exports.pastry_view_all_Page = async function(req, res) { 
+    try{ 
+        thePastry = await Pastry.find(); 
+        res.render('pastry', { title: 'Pastry Search Results', results: thePastry }); 
     } 
     catch(err){ 
         res.status(500); 
--- routes/pastry.js
+++ routes/pastry.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('pastry', { title: 'Search Results pastry' });
-});
-
-module.exports = router;
+var express = require('express'); 
+const pastry_controlers= require('../controllers/pastry'); 
+var router = express.Router(); 
+ 
+/* GET pastrys */ 
+router.get('/', pastry_controlers.pastry_view_all_Page ); 
+module.exports = router; 
