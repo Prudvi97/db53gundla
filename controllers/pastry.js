23b5ba78bff5b1d65bdca602460824ace33258d5
Prudvi97 <89538003+Prudvi97@users.noreply.github.com>
2021-11-17 16:34:10 -0600

    update and delete

--- controllers/pastry.js
+++ controllers/pastry.js
@@ -121,3 +121,29 @@ exports.pastry_create_Page = function (req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+
+// Handle building the view for updating a costume. 
+// query provides the id 
+exports.pastry_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await Pastry.findById(req.query.id)
+        res.render('pastryupdate', { title: 'Pastry Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+exports.pastry_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Pastry.findById(req.query.id) 
+        res.render('pastrydelete', { title: 'Pastry Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/pastry.js
+++ routes/pastry.js
@@ -8,4 +8,6 @@ router.get('/', pastry_controlers.pastry_view_all_Page );
 router.get('/detail', pastry_controlers.pastry_view_one_Page); 
 /* GET create costume page */ 
 router.get('/create', pastry_controlers.pastry_create_Page); 
+router.get('/update', pastry_controlers.pastry_update_Page); 
+router.get('/delete', pastry_controlers.pastry_delete_Page); 
 module.exports = router; 
